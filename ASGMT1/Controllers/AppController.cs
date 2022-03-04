21a724b93b11997db6c14a5827a83a30260f9a64
Nevin Johnson <nevinjgy@gmail.com>
2022-03-03 22:35:19 -0400

    Add project files.

---
+++ ASGMT1/Controllers/AppController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ASGMT1.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASGMT1.Controllers
+{
+    public class AppController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(AppViewModel form)
+        {
+            ViewBag.UserOutput = form.UserInput;
+            return View();
+        }
+    }
+}
